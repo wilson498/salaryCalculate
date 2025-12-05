70fac587f4f3bb6daf3650a6f334c37a688a93a3
WenJanzeMac <zxc80376@gmail.com>
2025-12-05 21:44:41 +0800

    Refactor `Salary` and `SalaryCalculate` to simplify salary computation by consolidating logic into `Salary.getSalaryActual`.

--- src/main/java/com/example/demo/Salary.java
+++ src/main/java/com/example/demo/Salary.java
@@ -2,4 +2,7 @@ package com.example.demo;
 
 public record Salary(int value) {
 
+    public int getSalaryActual(long monthDays, int leaveDays) {
+        return (int) (value - value * leaveDays / monthDays);
+    }
 }
--- src/main/java/com/example/demo/SalaryCalculate.java
+++ src/main/java/com/example/demo/SalaryCalculate.java
@@ -15,24 +15,17 @@ public class SalaryCalculate {
     private final LeaveRepo leaveRepo;
 
     public int calculate(int employeeId, int year, int month) {
-
         LocalDate startMonth = LocalDate.of(year, month, 1);
         LocalDate endMonth = startMonth.withDayOfMonth(startMonth.lengthOfMonth());
         long monthDays = ChronoUnit.DAYS.between(startMonth, endMonth) + 1;
-        int salary = salaryRepo.findByEmployeeId(employeeId).value();
 
+        Salary salary = salaryRepo.findByEmployeeId(employeeId);
         List<LeaveDate> leaveDates = leaveRepo.findAllByEmployeeId(employeeId);
         LeaveCalculate leaveCalculate = new LeaveCalculate(leaveDates);
 
-
         int leaveDays = leaveCalculate.getLeaveDays(monthDays, startMonth);
 
-
-        return getAnInt(salary, monthDays, leaveDays);
-    }
-
-    private int getAnInt(int salary, long monthDays, int leaveDays) {
-        return (int) (salary - (salary / monthDays) * leaveDays);
+        return salary.getSalaryActual(monthDays,leaveDays);
     }
 
 
